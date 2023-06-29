52d34c8287c32135c03dbbfbd463e06e5fa0ba52
vadym-loik <loikfit@gmail.com>
2023-06-29 18:55:39 +0200

    Update Button.vue
    add svg for arrow icon

--- myFileTest/src/components/Button.vue
+++ myFileTest/src/components/Button.vue
@@ -1,6 +1,11 @@
 <template>
     <button class="btn">
-        <i class="ri-arrow-down-line"></i>
+        <!-- <i class="ri-arrow-down-line"></i> -->
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="22" height="22">
+            <path
+                d="M12.9999 16.1716L18.3638 10.8076L19.778 12.2218L11.9999 20L4.22168 12.2218L5.63589 10.8076L10.9999 16.1716V4H12.9999V16.1716Z">
+            </path>
+        </svg>
     </button>
 </template>
 
